be058d5699c8c8360ca919893d2f1ae5e1f02976
Ole Tytlandsvik <ole.tytlandsvik@gmail.com>
2024-06-16 12:31:57 +0200

    feat: regreet dark theme

--- hosts/common/optional/regreet.nix
+++ hosts/common/optional/regreet.nix
@@ -1,4 +1,13 @@
 { ... }:
 {
-  programs.regreet.enable = true; # Login manager
+  # Login manager
+  programs.regreet = {
+    enable = true;
+    settings = {
+      GTK = {
+        application_prefer_dark_theme = true;
+        theme_name = "Adwaita";
+      };
+    };
+  };
 }
