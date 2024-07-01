9e4a2e43cf5440001fbc0d34c8664bb6a7a2521e
Ole Tytlandsvik <ole.tytlandsvik@gmail.com>
2024-07-01 18:47:48 +0200

    feat: laptop power button

--- hosts/common/optional/power.nix
+++ hosts/common/optional/power.nix
@@ -3,4 +3,7 @@
   # Power management settings for systems with a battery
   services.tlp.enable = true;
   services.thermald.enable = true;
+
+  # Avoid shutting down on power button short press
+  services.logind.powerKey = "ignore";
 }
