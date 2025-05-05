ed3cb02fdfb86d5dec78f183ba8cf7d4663c40cc
Ole Tytlandsvik <ole.tytlandsvik@gmail.com>
2025-05-05 17:39:32 +0200

    feat(power): enable powersave governor

--- hosts/common/optional/power.nix
+++ hosts/common/optional/power.nix
@@ -3,6 +3,8 @@
   # Power management settings for systems with a battery
   services.tlp.enable = true;
   services.thermald.enable = true;
+  # Could also be 'schedutil'
+  powerManagement.cpuFreqGovernor = "powersave";
 
   # Avoid shutting down on power button short press
   services.logind.powerKey = "ignore";
