da7c5c956bc684df35923fb478c29f331ec0b426
Ole Tytlandsvik <ole.tytlandsvik@gmail.com>
2026-04-08 09:12:20 +0200

    fix: switch power management services

--- hosts/common/optional/power.nix
+++ hosts/common/optional/power.nix
@@ -1,8 +1,11 @@
 { ... }:
 {
   # Power management settings for systems with a battery
-  services.tlp.enable = true;
+  # services.tlp.enable = true;
   services.thermald.enable = true;
+  services.power-profiles-daemon.enable = true;
+  # Noctalia wants upower for its battery UI
+  services.upower.enable = true;
   # Could also be 'schedutil'
   powerManagement.cpuFreqGovernor = "powersave";
 
